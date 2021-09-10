7dece65aac5a9caaf39815fb07966a9a872a4475
yoon <yunyun40550082@gmail.com>
2021-09-10 15:20:56 +0900

    Style: 목록에 포인터 속성 추가

--- src/components/sidebar/PageList.js
+++ src/components/sidebar/PageList.js
@@ -22,71 +22,53 @@ export default function PageList({
   const createTreeView = (pages, ul) => {
     ul.innerHTML = '';
     pages.map(page => {
-      const li = document.createElement('li');
-      li.classList.add('page');
-      li.dataset.pageid = page.id;
-      ul.appendChild(li);
+      if ($pageList.querySelector(`li[data-id="${page.id}"]`) === null) {
+        const li = document.createElement('li');
+        li.classList.add('page');
+        li.dataset.pageid = page.id;
+        ul.appendChild(li);
 
-      li.innerHTML = `
-        <div class="page_focuable_elements">
-              <button class="page_toggleButton" type="button">
-                <i class="fas fa-caret-right"></i>
-              </button>
-              <h3 class="page_name">${
-                page.title ? page.title : '제목 없음'
-              }</h3>
-              <button class="page_removeButton" type="button">
-                  𝗫
-              </button>
-              <button class="page_add_pageButton" type="button">
-                  ➕
-              </button>
-        </div>
-      `;
-      const children = document.createElement('ul');
-      children.classList.add('page_list');
-      children.classList.add('visible');
-      li.appendChild(children);
+        li.innerHTML = `
+          <div class="page_focuable_elements">
+                <button class="page_toggleButton" type="button">
+                  <i class="fas fa-caret-down"></i>
+                </button>
+                <h3 class="page_name">${
+                  page.title ? page.title : '제목 없음'
+                }</h3>
+                <button class="page_removeButton" type="button">
+                    𝗫
+                </button>
+                <button class="page_add_pageButton" type="button">
+                    ➕
+                </button>
+          </div>
+        `;
+        const children = document.createElement('ul');
+        children.classList.add('page_list');
+        children.classList.add('visible');
+        li.appendChild(children);
 
-      if (page.documents.length) {
-        createTreeView(page.documents, children);
+        if (page.documents.length) {
+          createTreeView(page.documents, children);
+        }
       }
     });
   };
 
-  // fa - caret - down;
-
-  const changeToggleIcon = () => {
-    const openChildrens = $rootUl.querySelectorAll('.visible');
-    for (const children of openChildrens) {
-      const toggleIcon = children.querySelector('.fas');
-      if (toggleIcon) {
-        toggleIcon.classList.remove('fa-caret-right');
-        toggleIcon.classList.add('fa-caret-down');
-      }
-    }
-
-    // .classList.replace('fa-caret-right', 'fa-caret-down');
-    // if (target.classList.contains('fa-caret-right')) {
-    //   target.classList.replace('fa-caret-right', 'fa-caret-down');
-    // } else if (target.classList.contains('fa-caret-down')) {
-    //   target.classList.replace('fa-caret-down', 'fa-caret-right');
-    // }
-  };
-
   this.render = () => {
     createTreeView(this.state, $rootUl); // 트리 목록 만든 후
-    changeToggleIcon(); // 자식 열려있는 것들 다 찾아서 토글 아이콘 바꿔줌
+    // changeToggleIcon(); // 자식 열려있는 것들 다 찾아서 토글 아이콘 바꿔줌
     $pageList.appendChild($rootUl); // 부모에 연결
   };
 
-  // const changeToggleIcon = target => {
-  //   if (target.classList.contains('fa-caret-right')) {
-  //     target.classList.replace('fa-caret-right', 'fa-caret-down');
-  //   } else if (target.classList.contains('fa-caret-down')) {
-  //     target.classList.replace('fa-caret-down', 'fa-caret-right');
-  //   }
-  // };
+  const changeToggleIcon = target => {
+    if (target.classList.contains('fa-caret-right')) {
+      target.classList.replace('fa-caret-right', 'fa-caret-down');
+    } else if (target.classList.contains('fa-caret-down')) {
+      target.classList.replace('fa-caret-down', 'fa-caret-right');
+    }
+  };
 
   $pageList.addEventListener('click', e => {
     const { target } = e;
@@ -105,7 +87,7 @@ export default function PageList({
     } else if (target.closest('.page_toggleButton')) {
       const children = $pageBlock.querySelector('.page_list');
       children.classList.toggle('visible');
-      // changeToggleIcon(target);
+      changeToggleIcon(target);
     } else if (className === 'page_removeButton') {
       onRemove(pageid);
     } else if (className === 'page_add_pageButton') {
