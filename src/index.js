ad2d27c7d62ede3e0fd2e0cc39a1e6eb47d67835
yoon <yunyun40550082@gmail.com>
2022-04-11 12:18:35 +0900

    build: webpack 설치 및 설정 세팅

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import App from './App.js';
+import '../style/index.scss';
+
+const $target = document.querySelector('#app');
+
+new App({
+  $target,
+});
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import App from './App.js';
-
-const $target = document.querySelector('#notion_app');
-
-// eslint-disable-next-line no-new
-new App({
-  $target,
-});
